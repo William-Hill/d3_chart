743bf5c21966096dfbd0474cc4969ef722118ea6
William Hill <hill119@llnl.gov>
2019-06-04 11:02:21 -0700

    extracted functions and added docstrings

--- parallel_coordinates.js
+++ parallel_coordinates.js
@@ -72,6 +72,12 @@ function createValueScale(variables, data, height) {
   return valueScale;
 }
 
+/**
+ * @description Create a scale to map climate variable names to a point between 0 and the width of the SVG container
+ * @param variables An Array containing the Variable names
+ * @param height The width of the SVG container
+ * @returns Object - A d3 scale
+ */
 function createModelScale(variables, width) {
   return d3
     .scalePoint()
@@ -80,7 +86,15 @@ function createModelScale(variables, width) {
     .domain(variables);
 }
 
-function plotPoints(data, x, y, symbolScale, colorScale) {
+/**
+ * @description Plots the symbols on the Parallel Coordinate visualization
+ * @param data An object representing the dataset that was parsed by D3.
+ * @param x A d3 scale to map climate variable names to a point between 0 and the width of the SVG container
+ * @param y an object with each climate variable as a key and a linear scale to calculate y axis position as a value
+ * @param symbolScale an ordinal scale to map climate models to a d3 shape
+ * @param colorScale an ordinal scale to map climate models to unique colors
+ */
+function plotSymbols(data, x, y, symbolScale, colorScale) {
   for (let row in data) {
     if (!isNaN(row)) {
       calculatePoint(data[row], x, y, symbolScale, colorScale);
@@ -88,6 +102,14 @@ function plotPoints(data, x, y, symbolScale, colorScale) {
   }
 }
 
+/**
+ * @description Calculate the position of a symbol on the Parallel Coordinate visualization
+ * @param row An object representing a row of the dataset that was parsed by D3.
+ * @param x A d3 scale to map climate variable names to a point between 0 and the width of the SVG container
+ * @param y an object with each climate variable as a key and a linear scale to calculate y axis position as a value
+ * @param symbolScale an ordinal scale to map climate models to a d3 shape
+ * @param colorScale an ordinal scale to map climate models to unique colors
+ */
 function calculatePoint(row, x, y, symbolScale, colorScale) {
   let row_model_name = row["model_name"];
   for (const [variable, value] of Object.entries(row)) {
@@ -114,19 +136,12 @@ function calculatePoint(row, x, y, symbolScale, colorScale) {
   }
 }
 
-function calculatePath(data) {
-  return d3.line()(function(data) {
-    for (const [variable, value] of Object.entries(row)) {
-      if (variable == "model_name") {
-        continue;
-      }
-      let xPoint = x(variable);
-      let yPoint = y[variable](value);
-      return [xPoint, yPoint];
-    }
-  });
-}
-
+/**
+ * @description Create a legend to show the symbol:model pairings
+ * @param model_names An array of model names from the dataset
+ * @param symbolScale an ordinal scale to map climate models to a d3 shape
+ * @param colorScale an ordinal scale to map climate models to unique colors
+ */
 function createLegend(model_names, symbolScale, colorScale) {
   let legend = d3.select("#legendColumns");
 
@@ -178,46 +193,21 @@ function createLegend(model_names, symbolScale, colorScale) {
       .text(d);
   });
 }
-// Parse the Data
-d3.csv("mycsvfile.csv", function(data) {
-  // Extract the list of variables we want to keep in the plot. Here I keep all except the column called model_name
-  console.log("data:", data);
-  const variables = getVariables(data);
 
-  const model_names = data.map(d => d["model_name"]);
-
-  let colorScale = createColorScale(model_names);
-
-  // For each dimension, I build a linear scale. I store all in a y object
-  let y = createValueScale(variables, data, height);
-  console.log("y:", y);
-
-  // Build the X scale -> it find the best position for each Y axisLeft
-  let x = createModelScale(variables, parentDiv.clientWidth);
-  console.log("x:", x);
-
-  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-  function path(d) {
-    console.log("d in path function:", d);
-    return d3.line()(
-      variables.map(function(p) {
-        console.log("p:", p);
-        console.log(`${p} -> x(p) in path function:`, x(p));
-        console.log(`${p} for y[p](d(p)) in path function:`, y[p](d[p]));
-        return [x(p), y[p](d[p])];
-      })
-    );
-  }
-
-  let colorScale_3 = d3.scaleSequential(d3.interpolateViridis);
-
+/**
+ * @description Draw a SVG path for each climate model's values
+ * @param data An object representing the dataset that was parsed by D3.
+ * @param calculatePath a callback function to calculate a SVG path through the values of each Variable of a climate model
+ */
+function drawCoordinateLines(data, calculatePath) {
   // Draw the lines
+  let colorScale_3 = d3.scaleSequential(d3.interpolateViridis);
   svg
     .selectAll("myPath")
     .data(data)
     .enter()
     .append("path")
-    .attr("d", path)
+    .attr("d", calculatePath)
     .style("fill", "none")
     .style("stroke", function(d, i) {
       return colorScale_3(i / data.length);
@@ -225,12 +215,26 @@ d3.csv("mycsvfile.csv", function(data) {
     .attr("class", function(d) {
       return "path_regular coordinate_path " + d["model_name"];
     });
+}
 
-  let symbolScale = d3.scaleOrdinal(d3.symbols);
+/**
+ * @description Create a text data table for the dataset
+ * @param data An object representing the dataset that was parsed by D3.
+ */
+function createTable(data) {
+  let grid = d3.divgrid();
+  d3.select("#grid")
+    .datum(data)
+    .call(grid);
+}
 
-  plotPoints(data, x, y, symbolScale, colorScale);
-  createLegend(model_names, symbolScale, colorScale);
-
+/**
+ * @description Draw the axes for the Parallel Coordinate visualization
+ * @param variables An Array containing the Variable names
+ * @param x A d3 scale to map climate variable names to a point between 0 and the width of the SVG container
+ * @param y an object with each climate variable as a key and a linear scale to calculate y axis position as a value
+ */
+function drawAxis(variables, x, y) {
   // Draw the axis:
   svg
     .selectAll("myAxis")
@@ -254,9 +258,54 @@ d3.csv("mycsvfile.csv", function(data) {
       return d;
     })
     .style("fill", "black");
+}
 
-  var grid = d3.divgrid();
-  d3.select("#grid")
-    .datum(data)
-    .call(grid);
+// Parse the Data
+d3.csv("mycsvfile.csv", function(data) {
+  // Extract the list of variables we want to keep in the plot. Here I keep all except the column called model_name
+  console.log("data:", data);
+  const variables = getVariables(data);
+
+  const model_names = data.map(d => d["model_name"]);
+
+  let colorScale = createColorScale(model_names);
+
+  // For each dimension, I build a linear scale. I store all in a y object
+  let y = createValueScale(variables, data, height);
+  console.log("y:", y);
+
+  // Build the X scale -> it find the best position for each Y axisLeft
+  let x = createModelScale(variables, parentDiv.clientWidth);
+  console.log("x:", x);
+
+  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+  function calculatePath(row) {
+    console.log("row in path function:", row);
+    return d3.line()(
+      variables.map(function(variable) {
+        console.log("variable:", variable);
+        console.log(
+          `${variable} -> x(variable) in path function:`,
+          x(variable)
+        );
+        let variableValue = row[variable];
+        console.log(
+          `${variable} for y[variable](variableValue) in path function:`,
+          y[variable](variableValue)
+        );
+        return [x(variable), y[variable](variableValue)];
+      })
+    );
+  }
+
+  drawCoordinateLines(data, calculatePath);
+
+  let symbolScale = d3.scaleOrdinal(d3.symbols);
+
+  plotSymbols(data, x, y, symbolScale, colorScale);
+  createLegend(model_names, symbolScale, colorScale);
+
+  drawAxis(variables, x, y);
+
+  createTable(data);
 });
